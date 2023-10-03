ca9f59771bae124fa2e6acee40667b556bc24bd6
mirshod948 <74175984+mirshod948@users.noreply.github.com>
2023-10-03 22:09:43 +0500

    task wather

---
+++ config/weather.php
@@ -0,0 +1,19 @@
+<?php
+
+
+return [
+    'providers' => [
+        'open-weather-map' => [
+            'api_key' => 'b34a93c573a4053fd27c4cd11b044756',
+            'url' => 'https://api.openweathermap.org/geo/1.0/direct',
+        ],
+        'accu-weather' => [
+            'api_key' => env('ACCU_WEATHER_API_KEY'),
+            'url' => 'https://api.example.com/accu-weather',
+        ],
+        'dark-sky' => [
+            'api_key' => env('DARK_SKY_API_KEY'),
+            'url' => 'https://api.example.com/dark-sky',
+        ],
+    ],
+];
